d01d57ff1004c3e570a920c65acdcb1a0e5b54c8
Shiwanshu Singh <144357090+shiwanshu1@users.noreply.github.com>
2024-05-19 22:44:23 +0530

    Add files via upload

---
+++ langchain_helper.py
@@ -0,0 +1,52 @@
+from langchain.llms import GooglePalm
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+import os
+
+api_key = "AIzaSyDNSjL9LLSsGuqVdVIntcaTfwUbYrtV54k"
+llm = GooglePalm(google_api_key=api_key, temperature=0.6)
+
+instructer_embeddings = HuggingFaceInstructEmbeddings()
+vectordb_file_path = "faiss_index"
+
+
+def create_vector_db():
+    loader = CSVLoader(file_path="codebasics_faqs.csv", source_column="prompt")
+    data = loader.load()
+    vectordb = FAISS.from_documents(documents=data, embedding=instructer_embeddings)
+    vectordb.save_local(vectordb_file_path)
+def get_qa_chain():
+    # Load the vector database from the local folder
+    vectordb = FAISS.load_local(vectordb_file_path, instructer_embeddings)
+
+    # Create a retriever for querying the vector database
+    retriever = vectordb.as_retriever(score_threshold=0.7)
+
+    prompt_template = """Given the following context and a question, generate an answer based on this context only.
+    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+    CONTEXT: {context}
+
+    QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type="stuff",
+                                        retriever=retriever,
+                                        input_key="query",
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt": PROMPT})
+
+    return chain
+
+if __name__ == "__main__":
+    create_vector_db()
+    chain = get_qa_chain()
+    print(chain("do you have any java fullstack course?"))
